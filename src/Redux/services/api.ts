e7bbf80c87616918a5fa8f002770f81212eb70f1
Brenda Antunes <brenda.antunes16@gmail.com>
2026-01-07 10:34:18 -0300

    Up api

--- src/Redux/services/api.ts
+++ src/Redux/services/api.ts
@@ -2,15 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-production-e48d.up.railway.app/'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/restaurantes'
   }),
 
   endpoints: (builder) => ({
     getRestaurantOptions: builder.query<Restaurant[], void>({
-      query: () => 'restaurantes'
+      query: () => ''
     }),
     getRestaurantData: builder.query<Restaurant, string>({
-      query: (id) => `restaurantes/${id}`
+      query: (id) => `${id}`
     }),
     purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
       query: (body) => ({
